43b1c10a9f391af5b536287b67a38c8b432413be
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-02-22 16:01:22 -0600

    replaced static consts with #define to avoid confusion of the purpose of the static consts

--- Core/Inc/mpu6050.h
+++ Core/Inc/mpu6050.h
@@ -13,21 +13,21 @@
 
 
 
-#define MPU6050_INT_PIN GPIO_PIN_0
+
 
 
 
 //extern volatile int mpu6050_data_ready;
 
-
-static const uint8_t MPU6050_ADDR = 0x68<<1;
-static const uint8_t WHO_AM_I = 0x75;
-static const uint8_t ACCEL_ZOUT_L = 0x40;
-static const uint8_t ACCEL_ZOUT_H = 0x3F;
-static const uint8_t ACCEL_XOUT_L = 0x3C;
-static const uint8_t ACCEL_XOUT_H = 0x3B;
-static const uint8_t ACCEL_CONFIG = 0x1C;
-static const uint8_t MPU_6050_INT_EN_REG = 0x38;
+#define MPU6050_INT_PIN GPIO_PIN_0
+#define MPU6050_ADDR (0x68<<1);
+#define WHO_AM_I 0x75;
+#define ACCEL_ZOUT_L 0x40;
+#define ACCEL_ZOUT_H 0x3F;
+#define ACCEL_XOUT_L 0x3C;
+#define ACCEL_XOUT_H 0x3B;
+#define ACCEL_CONFIG 0x1C;
+#define MPU_6050_INT_EN_REG 0x38;
 
 
 //void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
